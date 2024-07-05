adf1b74ec0e816a21e1249be8fb36f36c846e402
Siegerus <ivanoffsiegerus@gmail.com>
2024-07-05 19:50:09 +0300

    scores+adapt

--- src/blocks/modules/targets/targets.js
+++ src/blocks/modules/targets/targets.js
@@ -1,13 +1,19 @@
+"use strict";
+
 import $ from "jquery";
 import "../../../../node_modules/jquery/src/jquery-global";
 import "mediaelement";
 
 
-$(".targets__player").mediaelementplayer();
+$(".targets__player").mediaelementplayer({
+    
+});
 
-/* $(".targets__video").mediaelementplayer({
+/* $(".targets__player").mediaelementplayer({
 	
-    success: function(mediaElement, originalNode, instance) {}
+    success: function(mediaElement, originalNode, instance) {
+    }
+    
 
 }); */
 
