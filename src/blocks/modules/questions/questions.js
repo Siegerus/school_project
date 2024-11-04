c97132ad56b828544966c94f8c2f72b98c59af0f
Siegerus <ivanoffsiegerus@gmail.com>
2024-11-04 08:05:10 +0300

    1

--- src/blocks/modules/questions/questions.js
+++ src/blocks/modules/questions/questions.js
@@ -1,22 +1,3 @@
-/* "use strict";
-import $ from "jquery";
-
-$(document).ready(function() {
-
-    $(".questions-list__plus").each(function(i) {
-        $(this).on("click", function(){
-            
-            $(".questions__content-box").eq(i).toggleClass("questions__content-box_active");
-            $(".questions-list__item_content").eq(i).toggleClass("questions-list__item_content_active");
-        });
-
-        $(".questions-list__wrapper_content").eq(i).on("click", function() {
-            $(".questions-list__item_content").eq(i).removeClass("questions-list__item_content_active");
-            $(".questions__content-box").eq(i).removeClass("questions__content-box_active");
-        });
-    });
-}); */
-
 window.addEventListener("DOMContentLoaded", function () {
 
     "use strict";
